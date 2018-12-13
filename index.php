24aa55a3fd3737bc4f793768ae4a6bced48d9432
Tomislav <tomislav.gasparevic@outlook.de>
2018-12-13 10:31:45 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Salsa Vienna</title>
+    <link rel="stylesheet" href="assets/style.css"
+  </head>
+  <body>
+      <div id="container">
+        <div id="header">
+          <div class="event">
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
